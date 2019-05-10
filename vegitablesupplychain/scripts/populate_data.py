cfb5fe2768e6d7fe5a8fc43ddf949981c812605b
vsarode <vitthal.sarode1@gmail.com>
2019-05-10 08:40:20 +0530

    changes after discution

--- vegitablesupplychain/scripts/populate_data.py
+++ vegitablesupplychain/scripts/populate_data.py
@@ -32,19 +32,22 @@ def populate_products():
     _, _ = Product.objects.get_or_create(product_name='Potato',
                                          category=category1,
                                          brand=brand1,
-                                         default_image='https://en.wikipedia.org/wiki/Potato#/media/File:Bamberger_Hoernle.jpg',
+                                         default_image='potato.jpg',
+                                         # default_image='https://en.wikipedia.org/wiki/Potato#/media/File:Bamberger_Hoernle.jpg',
                                          features="1. Good \n 2. Best \n3.Better",
                                          price=102)
     _, _ = Product.objects.get_or_create(product_name='Tomato',
                                          category=category1,
                                          brand=brand1,
-                                         default_image='https://en.wikipedia.org/wiki/Tomato#/media/File:%E0%B0%9F%E0%B0%AE%E0%B0%BE%E0%B0%9F%E0%B0%BE%E0%B0%B2%E0%B1%81_(2).jpg',
+                                         default_image = 'tomato.jpg',
+                                         # default_image='https://en.wikipedia.org/wiki/Tomato#/media/File:%E0%B0%9F%E0%B0%AE%E0%B0%BE%E0%B0%9F%E0%B0%BE%E0%B0%B2%E0%B1%81_(2).jpg',
                                          features="1. Good \n 2. Best \n3.Better",
                                          price=102)
     _, _ = Product.objects.get_or_create(product_name='Bringle',
                                          category=category2,
                                          brand=brand2,
-                                         default_image='https://en.wikipedia.org/wiki/Eggplant#/media/File:Solanum_melongena_24_08_2012_(1).JPG',
+                                         default_image='bringle.jpg',
+                                         # default_image='https://en.wikipedia.org/wiki/Eggplant#/media/File:Solanum_melongena_24_08_2012_(1).JPG',
                                          features="1. Good \n 2. Best \n3.Better",
                                          price=102)
     print "Product populated successfully!!"
